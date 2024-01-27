ce8389eca5a96993386f14e21c497b289b5eba37
TanvirRahman888 <tanvirrahman8888@gmail.com>
2024-01-28 03:40:24 +0600

    Get Free Drinks.

---
+++ task1.js
@@ -0,0 +1,12 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+var burgerPrice =499;
+if (burgerPrice>500) {
+    console.log("Enjoy your burger with a free Drinks")
+} else {
+    console.log("Pay more 69 BDT for a soft Drinks")
+}
